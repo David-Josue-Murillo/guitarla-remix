dbc2fec626c52ada387962da40f50f6459fe163f
David Murillo <david.josue.murillo23@gmail.com>
2024-02-29 20:22:04 -0500

    Refactor code to improve performance and readability

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,27 @@
+import { Meta, Links } from '@remix-run/react'
+import style from './styles/index.css'
+
+export function meta() {
+   return (
+      {
+         charset: 'UTF-8',
+         title: 'GuitarLA - Remix',
+         viewport: 'width=device-width, initial-scale=1',
+         description: 'GuitarLA - Remix',
+         keywords: 'GuitarLA - Remix',
+      }
+   )
+}
+
+export function links() {
+   return [
+      {
+         rel: 'stylesheet',
+         href: style
+      }
+   ]
+} 
+
 export default function App() {
    return (
       <body>
@@ -5,14 +29,15 @@ export default function App() {
             <h1>Hola Mundo!</h1>
          </Document>
       </body>
-   );
+   )
 }
 
 function Document({ children }) {
    return (
       <html>
          <head>
-            <title>GuitarLA - Remix</title>
+            <Meta />
+            <Links />
          </head>
 
          <body>
